a664b291e48d173f13900bbb94505f5e6fe2148d
Mike Boyle <mikesboyle@gmail.com>
2015-12-02 15:20:52 -0500

    add step to remove nbsp

--- noDoubleSpace.js
+++ noDoubleSpace.js
@@ -4,7 +4,8 @@ Function.prototype.method = function(name, func) {
 };
 
 String.method('singleSpace', function() {
-  return this.replace(/[.|?|!]["|'|\u2019|\u201d]*\s{2,}/g, function(a, b) {
+  var text = this.replace(/\u00a0/g, ""); // get rid of &nbsp; that may have been converted
+  return text.replace(/[.|?|!]["|'|\u2019|\u201d]*\s{2,}/g, function(a, b) {
     var lastIndex = a[1] === " " ? 2 : 3;
     return a.substring(0, lastIndex);
   });
